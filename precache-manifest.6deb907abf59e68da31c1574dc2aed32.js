e786fea5d07d0237b8fb0c662dea84a0a0847761
paulfredmartin <paulmartin137@gmail.com>
2020-09-15 13:27:27 +0100

    Updates

---
+++ precache-manifest.6deb907abf59e68da31c1574dc2aed32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7125f241b7a6553ef3c8a0788ee84727",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5fc2e067ab36524eb368",
+    "url": "/monsters-rolodex/static/css/main.8a4b27f1.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fc2e067ab36524eb368",
+    "url": "/monsters-rolodex/static/js/main.d00eb850.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
